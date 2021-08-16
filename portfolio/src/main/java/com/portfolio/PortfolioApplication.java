2f5455ed805a1b65fddf50875f6d0973de70950b
ajaysagarn <ajaysagarn@gmail.com>
2021-08-15 19:45:59 -0500

    add dynamo-repo-service to spring scan

--- portfolio/src/main/java/com/portfolio/PortfolioApplication.java
+++ portfolio/src/main/java/com/portfolio/PortfolioApplication.java
@@ -2,14 +2,12 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication(scanBasePackages={"com.portfolio"})
-@EnableMongoRepositories(basePackages = "com.portfolio")
+@SpringBootApplication(scanBasePackages={"com.portfolio","com.ajsa.dyrepo"})
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
